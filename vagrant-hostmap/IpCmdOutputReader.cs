6f92bfdc8f763e1fa6eb26d1c31b792dec11b9b6
Avishkar Autar <aautar@fragmentspace.com>
2024-08-18 14:44:02 -0400

    execute simpler ip command on linux box for wider compatibility with bare-bones systems

---
+++ vagrant-hostmap/IpCmdOutputReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VagrantHostmap
+{
+    public class IpCmdOutputReader
+    {
+        static private string[] RemoveEmptyParts(string[] parts)
+        {
+            List<string> updatedParts = new List<string>();
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i].Trim().Length > 0)
+                {
+                    updatedParts.Add(parts[i].Trim());
+                }
+            }
+
+            return updatedParts.ToArray();
+        }
+
+        // Get interface names to IPv4 addresses from "ip -o -f inet addr" output
+        static public Dictionary<string, string> GetInterfaceIPAddressMapFromINetAddrOneLineOutput(string ipInetAddrOutput)
+        {
+            Dictionary<string, string> interfaceToIpAddr = new Dictionary<string, string>();
+
+            string[] lines = ipInetAddrOutput.Trim().Split('\n');
+            foreach (string ln in lines)
+            {
+                string[] parts = RemoveEmptyParts(ln.Split(null));
+                if (parts.Length < 4) // without empty parts, we expect at least 4 textual parts
+                {
+                    continue;
+                }
+
+                string interfaceName = parts[1].Trim(null).Trim(':');
+                string[] ipAddrParts = parts[3].Trim().Split('/');
+
+                interfaceToIpAddr.TryAdd(interfaceName, ipAddrParts[0].Trim());
+            }           
+
+            return interfaceToIpAddr;
+        }
+    }
+
+
+}
